1151ead3b4f0ad7a27a6dd56865d1ecb2551368f
oscarRickovic <med710scarrickovic@gmail.com>
2023-12-12 05:20:00 +0100

    create first cryptoGraphy middleware

--- BackEnd/CryptoMiddleWare/UserCryptoGraphyMiddleWare.js
+++ BackEnd/CryptoMiddleWare/UserCryptoGraphyMiddleWare.js
@@ -1,12 +1,14 @@
 const {encrypt, decrypt} = require('../Crypto/AsymCrypto');
 const sym = require('../Crypto/SymCrypto');
-const getCryptedRegisteringInformationFromClient = (req) => {
-    let clientPubKey = req.body.clientPubKey;
-    let data = req.body.data;
-    console.log(data);
-    let clearPrivateKey = sym.decrypt(keysJson.privateKey, process.env.SERVER_SECRET_KEY);
-    let clearData = decrypt(data, clearPrivateKey).split("00000000");
-    return {...clearData}
+const dotenv = require('dotenv');
+const keysJson = require('../Crypto/Keys.json');
+dotenv.config();
+const getUserRegister = (req) => {
+let clientPubKey = req.body.clientPubKey;
+  let data = req.body.data;
+  let clearPrivateKey = sym.decrypt(keysJson.privateKey, process.env.SERVER_SECRET_KEY);
+  let clearData = decrypt(data, clearPrivateKey).split("00000000");
+  return {...clearData}
 }
 
-module.exports = {getCryptedRegisteringInformationFromClient}
+module.exports = getUserRegister
--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -6,6 +6,7 @@ const generateServerKeys = require('./Crypto/ServerKeys');
 const keysJson = require('./Crypto/Keys.json');
 const {encrypt, decrypt} = require('./Crypto/AsymCrypto');
 const sym = require('./Crypto/SymCrypto');
+const getUserRegister = require('./CryptoMiddleWare/UserCryptoGraphyMiddleWare');
 dotenv.config() ;
 const PORT = process.env.PORT_BACK || 6000;
 const usersRoutes = require('./routes/users')
@@ -22,18 +23,12 @@ app.use('/users', usersRoutes)
 // All this behaviors should be splited in other files to more organise the code.
 app.get('/testCrypto', (req, res)=>{
   // In this step we should return the server public key that is stored in keys.json.
-  console.log(keysJson.publicKey)
   res.send(keysJson.publicKey);
 })
 
 // This is an example of CryptoGraphy middleWare
 app.post('/testCrypto', (req, res) => {
-  let clientPubKey = req.body.clientPubKey;
-  let data = req.body.data;
-  console.log(data);
-  let clearPrivateKey = sym.decrypt(keysJson.privateKey, process.env.SERVER_SECRET_KEY);
-  let clearData = decrypt(data, clearPrivateKey).split("00000000");
-  console.log({...clearData})
+  console.log(getUserRegister(req))
 })
 
 
