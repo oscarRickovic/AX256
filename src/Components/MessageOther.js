991194d4506538c6b8d4a2c6428d2fb5a9a1e58e
oscarRickovic <med710scarrickovic@gmail.com>
2023-12-27 03:22:29 +0100

    message design is responsive now with the size of message

--- src/Components/MessageOther.js
+++ src/Components/MessageOther.js
@@ -3,14 +3,10 @@ import './ComponentsCss/MessageOtherCss.css'
 import { Avatar } from '@mui/material'
 function MessageOther({msg}) {
   return (
-    <div className = "MessageOther">
-        <div className='MessageOther-Icon'>
-            <Avatar alt="Remy Sharp" src="https://wallpapers-clan.com/wp-content/uploads/2023/05/cute-anime-boy-art-wallpaper.jpg" />
-        </div>
         <div className='MessageOther-Msg'>
             {msg}
         </div>
-    </div>
+
   )
 }
 
