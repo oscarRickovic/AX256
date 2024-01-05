6a4842087a5a4add7f4a3027b361d22a02d9c4f4
oscarRickovic <med710scarrickovic@gmail.com>
2024-01-05 16:11:44 +0100

    fetching friends is passed to be in conversations not in SideBar

--- src/Components/Conversations.js
+++ src/Components/Conversations.js
@@ -1,13 +1,36 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import "./ComponentsCss/ConversationsCss.css"
 import ConversationElement from './ConversationElement'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import sendCryptedData from './StaticFunctions/SendingCryptedDataToServer';
+import ErrorGettingFriends from './ErrorGettingFriends';
+import NoChatSelected from './NoChatSelected';
 
-function Conversations({users}) {
+function Conversations() {
   const navigate = useNavigate();
   const light = useSelector((state) => state.LightState.value);
   const socket = useSelector(state => state.SocketState.value);
+  const renderConversation = useSelector((state) => state.RendersState.Conversations)
+  const [error, setError] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    const getUsers = async () => {
+      try {
+        let response = await sendCryptedData('GET', `${process.env.REACT_APP_URL}/user`);
+        if(response.status == 200) {
+          setUsers(response.data);
+        }
+        else {
+          setError(true);
+        }
+      } catch(e) {
+        setError(true)
+      }
+    }
+    getUsers();
+  },[renderConversation])
 
   // Connect all the sockets with all Conversations.
   useEffect(()=>{
@@ -22,19 +45,28 @@ function Conversations({users}) {
   },[socket, users])
 
   return (
-    <div className='Conversations'>
-      {light}
-      {users.map( user => {
-        
-        return (
-        <div key={user.friendShipId} onClick={() => {navigate('/app/chat/' + user.friendShipId)}}>
-          <ConversationElement user={user.friendInfo} keyChat={user.friendShipId}/>
-        </div>
-        )
-      })}
-      
-    </div>
+    <>
+      {users
+        ?
+      <div className='Conversations'>
+        {light}
+        {users.map( user => {
+          return (
+            <div key={user.friendShipId} onClick={() => {navigate('/app/chat/' + user.friendShipId)}}>
+            <ConversationElement user={user.friendInfo} keyChat={user.friendShipId}/>
+          </div>
+          )
+        })}
+      </div>
+        :
+        (error) 
+        ? 
+        <ErrorGettingFriends/>
+        : 
+        <NoChatSelected rotation = {true}/>
+    }
+    </>
   )
 }
 
-export default Conversations
+export default Conversations;
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,38 +1,16 @@
-import React,{useEffect, useState} from 'react'
+import React from 'react'
 import './ComponentsCss/SideBarCss.css'
 import SideBarSearch from './SideBarSearch';
 import Conversations from './Conversations';
 import { useSelector} from 'react-redux'
-import ErrorGettingFriends from './ErrorGettingFriends';
 import SideBarSettings from './SideBarSettings';
 import NoChatSelected from './NoChatSelected';
-import axios from 'axios';
-import sendCryptedData from './StaticFunctions/SendingCryptedDataToServer';
 
 function SideBar() {
     const light = useSelector(state=>state.LightState.value);
     let color = useSelector(state => state.ColorState.second);
     let findNewFriends = useSelector(state => state.PassNewFriendState.value);
-    let renderConversation = useSelector((state) => state.RendersState.Conversations)
-    const [error, setError] = useState(false);
-    const [isPending, setIsPending] = useState(false);
-    const [users, setUsers] = useState([]);
-    useEffect(()=>{
-      const getUsers = async () => {
-        try {
-          let response = await sendCryptedData('GET', `${process.env.REACT_APP_URL}/user`);
-          if(response.status == 200) {
-            setUsers(response.data);
-          }
-          else {
-            setError(true);
-          }
-        } catch(e) {
-          setError(true)
-        }
-      }
-      getUsers();
-    },[renderConversation])
+    
   return (
     //Streotype : 
     // MyButton we will pass :
@@ -46,7 +24,7 @@ function SideBar() {
         <>
             <SideBarSettings/>
             <SideBarSearch/>
-            { users ? <Conversations users={users} /> : (error) ? <ErrorGettingFriends/> : <NoChatSelected rotation = {true}/> }
+            <Conversations/>
         </> 
         :
         <NoChatSelected rotation = {false}/>}
